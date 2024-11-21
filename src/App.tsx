8c03a4228bf5aefe12c492448f2e0ac825f24d0d
Nahuel Castro <nahukas@gmail.com>
2024-11-21 14:16:22 +0100

    front: add api simulation

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import './App.css';
 import CountrySelector from './components/CountrySelector/CountrySelector';
 import $Title from './components/Title/Title';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}
-    >
-      <$Title>Select a Country</$Title>
-      <CountrySelector />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <$Title>Select a Country</$Title>
+        <CountrySelector />
+      </div>
+    </QueryClientProvider>
   );
 }
 
