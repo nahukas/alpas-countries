bb02ad52a7ab6f7856b7a8c031ad05aed29b26f2
Nahuel Castro <nahukas@gmail.com>
2024-11-21 12:53:39 +0100

    front: add vitest and hook unit test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { configDefaults } from 'vitest/config';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+    css: true
+  }
+});
